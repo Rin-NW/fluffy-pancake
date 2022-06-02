5cd9d35a542f41e5bc28fff66b0ce56600a1c822
Harvey <flandrescarlet495y@qq.com>
2022-06-02 17:03:37 +0800

    Create package.json

--- src/index.js
+++ src/index.js
@@ -8,9 +8,52 @@ function randFace() {
 	return ['Crown', 'Anchor', 'Heart', 'Spade', 'Club', 'Diamond'][rand(0,5)];
 }
 
-let funds = 50;
-let round = 0;
 
+for (let funds = 50, round = 0; funds > 1 && funds < 100; round++){
+	console.log(`Round: ${round}`);
+	console.log(`\tStarting funds: ${funds}`);
+	const bets = { Crown: 0, Anchor: 0, Heart: 0, Spade: 0, Club: 0, Diamond: 0 };
+	let totalBet = rand(1, funds);
+	if (totalBet === 7) {
+		totalBet = funds;
+		bets.Heart = totalBet;
+		console.log('Oh ~ I got 7 pence. All in Heart!');
+	} else {
+		let remaining = totalBet;
+		do {
+			let bet = rand(1, remaining);
+			let face = randFace();
+			bets[face] = bets[face] + bet;
+			console.log ('I would bet on : ' + face + ' with ' + bets[face] + 'p');
+			remaining = remaining - bet;
+		}	while (remaining > 0 );
+	}
+	funds = funds - totalBet;
+
+	console.log('\tBets result:' + Object.keys(bets).map( face => `${face}: ${bets[face]} pence`) + `(totalBet: ${totalBet})pence.`);
+
+	const hand = [];
+
+	for (let roll = 0; roll < 3; roll++) {
+		hand.push(randFace());
+	}
+
+	console.log(`\thand: ${hand.join(',')}`);
+
+	let winnings = 0;
+	for (let score = 0; score < hand.length; score++){
+		let face = hand[score];
+		if (bets[face] > 0) {
+			winnings = winnings + bets[face];
+		}
+	}
+
+	funds = funds + winnings;
+	console.log(`\tWinning: ${winnings}`);
+	console.log(`\tending funds: ${funds}`);
+
+}
+/* 
 while(funds > 1 && funds < 100) {
 	round++;
 	console.log(`Round: ${round}`);
@@ -55,3 +98,4 @@ while(funds > 1 && funds < 100) {
 	console.log(`\tWinning: ${winnings}`);
 	console.log(`\tending funds: ${funds}`);
 }
+*/
